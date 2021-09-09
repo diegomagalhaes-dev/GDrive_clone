fd82c7741edddbe3ce788d874246e06e0add5241
diegomagalhaes-dev <digengup@gmail.com>
2021-09-08 23:56:55 -0300

    :sparkles: defined routes structures

---
+++ aulas/aula01/gdrive-webapi/src/routes.js
@@ -0,0 +1,35 @@
+import { logger } from "./logger.js";
+
+export default class Routes {
+  io;
+  constructor() {}
+
+  setSocketInstance(io) {
+    this.io = io;
+  }
+
+  async defaultRoute(request, response) {
+    response.end("hello world");
+  }
+
+  async options(request, response) {
+    response.writeHead(204);
+    response.end("hello world");
+  }
+
+  async post(request, response) {
+    logger.info("ae");
+    response.end("post");
+  }
+
+  async get(request, response) {
+    response.end("get");
+  }
+
+  handler(request, response) {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    const chosen = this[request.method.toLowerCase()] || this.defaultRoute;
+
+    return chosen.apply(this, [request, response]);
+  }
+}
